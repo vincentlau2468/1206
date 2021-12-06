c59b12c3d445fd20be42c07b4faf9bad1d9713d0
vincentlau2468 <91239047+vincentlau2468@users.noreply.github.com>
2021-12-06 11:29:41 +0800

    Update Q4.c

--- Q4.c
+++ Q4.c
@@ -5,17 +5,15 @@ int main(void)
     char input[9999];
     char change;
     char ans; 
-    char eat;
     gets(input);
-    scanf("%c", &change);
-    scanf("%c",&eat);
-    scanf("%c", &ans);
+    scanf(" %c", &change);
+    scanf(" %c", &ans);
 
     for (int i = 0; i < strlen(input); i++)
     {
         if (change == input[i])
         {
             input[i] = ans;
-        }
+        }   
     }puts(input);
 }
